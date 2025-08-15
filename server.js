ec5a65593b99aa2c754de1a12f119bef7a264121
NeuroNex <neuronex493@gmail.com>
2025-08-15 13:17:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname)));
+const PORT = process.env.PORT || 3000;
+
+// Public folder serve करो
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
